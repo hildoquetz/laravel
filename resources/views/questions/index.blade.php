a70904b21c4645fd3e7b13531de387c02d65a78e
hildoquetz <hildoquetz@gmail.com>
2019-02-03 12:44:43 -0200

    update question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -32,11 +32,17 @@
                             </div>
                         </div>
                         <div class="media-body">
-                            <h3 class="mt-0">
-                                <a href="{{ route("questions.show", $question->id) }}">
-                                    {{ $question->title }}
-                                </a>
-                            </h3>
+                            <div class="d-flex align-items-center">
+                                <h3 class="mt-0">
+                                    <a href="{{ route("questions.show", $question->id) }}">
+                                        {{ $question->title }}
+                                    </a>
+                                </h3>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                </div>
+                            </div>
+                            
                             <p class="lead">
                                 Asked by 
                                 <a href="{{ route("questions.show", $question->user->id ) }}">{{ $question->user->name }}</a>
