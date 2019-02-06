842a671a90340afd8a44854e5cf603a1cf0413c4
hildoquetz <hildoquetz@gmail.com>
2019-02-06 06:37:11 -0200

    triger on save answer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 3)->create()->each(function($user){
-            $user->questions()->saveMany(
-                    factory(App\Question::class, rand(1,5))->make()
-                );
+                $user->questions()
+                ->saveMany(factory(App\Question::class, rand(1,5))->make())
+                ->each(function($question){
+                    $question->answers()->saveMany(factory(App\Answer::class, rand(1,5))->make());
+                });
         });
     }
 }
