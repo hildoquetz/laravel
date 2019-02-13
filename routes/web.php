17ff44bd920b0cbfde06872e4aeebb05ada10c09
hildoquetz <hildoquetz@gmail.com>
2019-02-13 03:26:09 -0200

    display answers for questions

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return redirect('/questions');
 });
 
 Auth::routes();
@@ -20,4 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
+Route::resource('questions.answers', 'AnswersController')->only(['store', 'edit', 'update', 'destroy']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show'); 
